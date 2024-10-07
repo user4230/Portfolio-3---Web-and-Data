1ddaf91477bb7d0201d2321b6ba1c01441d89bec
user4230 <nicholasgladik@hotmail.com>
2024-10-07 18:16:45 +0200

    created randomNumber

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 'use strict';
 
-const randomNumberGenerator = Math.floor(Math.random() * 20 + 1)
-console.log(randomNumberGenerator)
+const selectButtonCheck = document.querySelector("button.btn.check")
+console.log(selectButtonCheck)
+function checkPlayerInputCorrect() {
+    const randomNumberResult = Math.floor(Math.random() * 20 + 1)
+    const checkPlayerInput = document.querySelector("input")
+    const outputOfPlayer = checkPlayerInput.value
 
+    selectButtonCheck.addEventListener("click", () => {
+        console.log(outputOfPlayer)
+    });
 
+    if (randomNumberResult === outputOfPlayer) {
+        console.log("you win")
+    } else if (randomNumberResult != outputOfPlayer) {
+        console.log("you lose")
+    }
+}
+checkPlayerInputCorrect()
