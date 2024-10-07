2e80df453bbc772c2ab3ef649d0f4ff103f74569
user4230 <nicholasgladik@hotmail.com>
2024-10-08 00:39:30 +0200

    You are correct, you are wrong, number displayed working. Score and highScore are still bugged.

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
 'use strict';
 
-const selectButtonCheck = document.querySelector("button.btn.check")
-console.log(selectButtonCheck)
-function checkPlayerInputCorrect() {
-    const randomNumberResult = Math.floor(Math.random() * 20 + 1)
-    const checkPlayerInput = document.querySelector("input")
-    const outputOfPlayer = checkPlayerInput.value
 
-    selectButtonCheck.addEventListener("click", () => {
-        console.log(outputOfPlayer)
-    });
 
-    if (randomNumberResult === outputOfPlayer) {
-        console.log("you win")
-    } else if (randomNumberResult != outputOfPlayer) {
-        console.log("you lose")
+const randomNumberResult = Math.floor(Math.random() * 20 + 1);
+console.log(randomNumberResult)
+let selectPlayerInput = document.querySelector("input.guess");
+let highScore = 0;
+let score = 20;
+
+function checkIfPlayerCorrect() {
+document.querySelector("button.btn.check").addEventListener("click", () => {
+
+    let getValueFromPlayer = Number(selectPlayerInput.value);
+    let printMessage = document.querySelector("p.message");
+    const highScoreSelector = document.querySelector("span.highscore");
+    let scoreSelector = document.querySelector("span.score")
+
+    if (getValueFromPlayer === randomNumberResult) {
+        printMessage.textContent = "You are correct!";
+        document.querySelector(".number").textContent = randomNumberResult // prints correct result in "?"
+        selectPlayerInput.value = "" // clears input field
+        if (score < highScore) {
+            highScore = score;
+            highScoreSelector.textContent = highScore
+        }
+    } else {
+        printMessage.textContent = "You are wrong!";
+        scoreSelector = score--;
+        selectPlayerInput.value = ""
     }
+
+})
 }
-checkPlayerInputCorrect()
+checkIfPlayerCorrect()
+
+/*function changeHighScore () {
+            const selectHighScore = Number(document.querySelector(".span.highscore").textContent)
+            console.log(selectHighScore)
+            if (getValueFromPlayer < highScore) {
+
+            }
+        }*/
