09d5d936b270c4dd7f7d5d947468c7b421850835
user4230 <nicholasgladik@hotmail.com>
2024-10-08 19:31:54 +0200

    functionality in place, storeGuesses needs to be aligned in center of webpage, add confetti + sounds, add a fun feature

--- script.js
+++ script.js
@@ -4,40 +4,58 @@
 
 const randomNumberResult = Math.floor(Math.random() * 20 + 1);
 console.log(randomNumberResult)
-let selectPlayerInput = document.querySelector("input.guess");
-let highScore = 0;
-let score = 20;
 
-function checkIfPlayerCorrect() {
+const selectPlayerInput = document.querySelector("input.guess");
+const highScoreSelector = document.querySelector("span.highscore");
+const scoreSelector = document.querySelector("span.score")
+const body = document.body
+let score = 20;
+let highScore = 0;
+
+function checkIfGuessCorrect() {
 document.querySelector("button.btn.check").addEventListener("click", () => {
 
-    let getValueFromPlayer = Number(selectPlayerInput.value);
     let printMessage = document.querySelector("p.message");
-    const highScoreSelector = document.querySelector("span.highscore");
-    let scoreSelector = document.querySelector("span.score")
+    let getValueFromPlayer = Number(selectPlayerInput.value);
+
+    const storeGuesses = document.createElement('h2')
+    storeGuesses.style.display = 'flex';
+    storeGuesses.style.justifyContent = 'center';
+    storeGuesses.style.textAlign = 'center';
+    storeGuesses.innerHTML = "Guess:" + getValueFromPlayer
+    body.append(storeGuesses)
+
+    if (getValueFromPlayer < 1 || getValueFromPlayer > 20) {
+        printMessage.textContent = "Please use a number between 1-20!"
+        selectPlayerInput.value = "" // clears input field when invalid value is added
+        return;
+    }
 
     if (getValueFromPlayer === randomNumberResult) {
         printMessage.textContent = "You are correct!";
         document.querySelector(".number").textContent = randomNumberResult // prints correct result in "?"
-        selectPlayerInput.value = "" // clears input field
-        if (score < highScore) {
+
+        if (score > highScore) {
             highScore = score;
             highScoreSelector.textContent = highScore
         }
-    } else {
-        printMessage.textContent = "You are wrong!";
-        scoreSelector = score--;
-        selectPlayerInput.value = ""
-    }
 
+        } else {
+            printMessage.textContent = "You are wrong!";
+            score--;
+            scoreSelector.textContent = score;
+    }
+    selectPlayerInput.value = "" // clears input field
 })
 }
-checkIfPlayerCorrect()
+checkIfGuessCorrect();
 
-/*function changeHighScore () {
-            const selectHighScore = Number(document.querySelector(".span.highscore").textContent)
-            console.log(selectHighScore)
-            if (getValueFromPlayer < highScore) {
+function restartGameScore() {
+    document.querySelector("button.again").addEventListener("click", () => {
+        scoreSelector.textContent = score;
+        highScoreSelector.textContent = "0";
+        selectPlayerInput.value = ""
+    });
+}
+restartGameScore()
 
-            }
-        }*/
