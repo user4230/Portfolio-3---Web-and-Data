e1beb9677c1695fd500502dacaa95a7ec651b217
user4230 <nicholasgladik@hotmail.com>
2024-10-08 21:17:53 +0200

    functionality in place, storeGuesses needs to be aligned in center of webpage, confetti + gif added. Debugged for the most part

--- script.js
+++ script.js
@@ -11,51 +11,71 @@ const scoreSelector = document.querySelector("span.score")
 const body = document.body
 let score = 20;
 let highScore = 0;
+let playerHasWon = false;
 
-function checkIfGuessCorrect() {
+function ifNumberCorrect() {
 document.querySelector("button.btn.check").addEventListener("click", () => {
 
     let printMessage = document.querySelector("p.message");
-    let getValueFromPlayer = Number(selectPlayerInput.value);
+    let getValueFromPlayer = Number(selectPlayerInput.value)
 
-    const storeGuesses = document.createElement('h2')
+    if (selectPlayerInput.value === "") {
+        printMessage.textContent = "Please use a number!"
+        return;
+    }
+
+    // logs guesses made
+    const storeGuesses = document.createElement('span')
     storeGuesses.style.display = 'flex';
     storeGuesses.style.justifyContent = 'center';
-    storeGuesses.style.textAlign = 'center';
+    storeGuesses.style.fontSize = '3rem'
     storeGuesses.innerHTML = "Guess:" + getValueFromPlayer
     body.append(storeGuesses)
 
-    if (getValueFromPlayer < 1 || getValueFromPlayer > 20) {
+    // limits input between 1-20
+    if (getValueFromPlayer < 0 || getValueFromPlayer > 20) {
         printMessage.textContent = "Please use a number between 1-20!"
         selectPlayerInput.value = "" // clears input field when invalid value is added
         return;
     }
+    // if result is correct
     if (getValueFromPlayer === randomNumberResult) {
-        printMessage.textContent = "You are correct!";
+        printMessage.textContent = "You win!";
+        // replaces background with gif when win and adds confetti
         document.querySelector(".number").textContent = randomNumberResult // prints correct result in "?"
+        document.body.style.backgroundImage = 'url(https://data.textstudio.com/output/sample/animated/5/9/8/5/win-25-5895.gif)'
+        document.body.style.backgroundRepeat = 'no-repeat';
+        document.body.style.backgroundPosition = 'center';
+        document.body.style.backgroundSize = 'cover';
+        const jsConfetti = new JSConfetti()
+        jsConfetti.addConfetti()
+        playerHasWon = true;
 
+        // set score to highScore if higher than highScore
         if (score > highScore) {
             highScore = score;
             highScoreSelector.textContent = highScore
         }
-
+        // if user is wrong
         } else {
-            printMessage.textContent = "You are wrong!";
+            printMessage.textContent = "Try again!";
             score--;
             scoreSelector.textContent = score;
     }
     selectPlayerInput.value = "" // clears input field
 
+    // again button functionality
     function restartGameScore() {
         document.querySelector("button.again").addEventListener("click", () => {
             scoreSelector.textContent = 20;
             highScoreSelector.textContent = "0";
             selectPlayerInput.value = ""
             const removeGuesses = storeGuesses.remove()
+            document.body.style.backgroundImage = ''
         });
     }
     restartGameScore()
 
 })
 }
-checkIfGuessCorrect();
+ifNumberCorrect();
